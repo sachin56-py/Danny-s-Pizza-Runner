4e158595ea03a1cfead94d3417305dda4133831e
Sachin Karki <sachin56.py@gmail.com>
2024-09-20 11:24:14 +1000

    Remaining Questions

--- Pizza Metrics.sql
+++ Pizza Metrics.sql
@@ -47,9 +47,47 @@ select top 1
 	group by c.order_id
 	order by count(pizza_id) desc
 
+--7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+--using count
+select 
+	c.customer_id,
+	count(case when exclusions is not NULL or extras is not null then pizza_id end) as changed, 
+	count(case when exclusions is NULL and extras is null then pizza_id end) as no_changed 
+	from customer_orders c join runner_orders r 
+	on c.order_id=r.order_id 
+	where cancellation is NULL  
+	group by customer_id
 
-select top 2 * from runner_orders
-select * from customer_orders
-select * from pizza_toppings
-select * from pizza_recipes
-select * from pizza_names
+-- using sum
+select c.customer_id,
+sum(case when c.exclusions is not NULL or c.extras is not NULL then 1 else 0 end) as changed_pizzas,
+sum(case when c.exclusions is NULL and c.extras is NULL then 1 else 0 end) as unchanged_pizzas
+from customer_orders as c join runner_orders as r
+on c.order_id = r.order_id
+where r.cancellation is null
+group by c.customer_id;
+
+
+--8.How many pizzas were delivered that had both exclusions and extras?
+select 
+sum(case when c.exclusions is not NULL and c.extras is not NULL then 1 else 0 end) as having_extras_and_exclusions
+from customer_orders as c join runner_orders as r
+on c.order_id = r.order_id
+where r.cancellation is null
+
+--9. What was the total volume of pizzas ordered for each hour of the day?
+select
+	DATEPART(hour,order_time) as hours,
+	count(DATEPART(hour,order_time)) as pizza_volume
+	from customer_orders
+	group by DATEPART(hour,order_time)
+	order by count(DATEPART(hour,order_time)) desc
+
+--10.What was the volume of orders for each day of the week?
+SELECT 
+	DATENAME(dw,order_time) as day_of_week,
+	count(DATENAME(dw,order_time)) as pizza_volume
+	from customer_orders
+	group by DATENAME(dw,order_time)
+	order by count(DATENAME(dw,order_time)) desc
+
