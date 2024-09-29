5621b44418ac5be659526c3981665b0315bb8d2f
Sachin Karki <sachin56.py@gmail.com>
2024-09-29 10:08:08 +1000

    Runner and Customer Experiences Solutions

--- Runner and Customer Experience.sql
+++ Runner and Customer Experience.sql
@@ -1,22 +1,8 @@
 --1.How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
---select * from runners
-
-SELECT 
-    DATEADD(WEEK, DATEDIFF(WEEK, '2021-01-01', signup_date), '2021-01-01') AS week_start,
-    COUNT(*) AS runner_count
-FROM 
-    runners
-GROUP BY 
-    DATEADD(WEEK, DATEDIFF(WEEK, '2021-01-01', signup_date), '2021-01-01')
-ORDER BY 
-    week_start;
+-- RESOLVE THE ERROR
 
 
-select 
-	DATEPART(week,dateadd(day,-DATEDIFF(day,'2021-01-01','2020-12-28'),registration_date)) as week_number,
-	count(runner_id) as no_of_runner_registered
-	from runners
-	group by DATEPART(week,dateadd(day,-DATEDIFF(day,'2021-01-01','2020-12-28'),registration_date))
+
 
 
 --2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
@@ -27,3 +13,83 @@ FROM runner_orders r
 JOIN customer_orders c
     ON r.order_id = c.order_id
 	group by runner_id;
+
+--3.Is there any relationship between the number of pizzas and how long the order takes to prepare?
+--RESOLVE THE ERROR 
+select 
+	c.order_id, avg(cast(
+						replace(duration,'mins','') 
+						as decimal)) as duration
+	from runner_orders r join customer_orders c
+	on r.order_id = c.order_id
+	group by c.order_id;
+
+--4. What was the average distance travelled for each customer?
+select 
+	customer_id, round(
+					avg(
+						CAST(
+							REPLACE(distance, 'km', '') 
+							as decimal)
+						)
+					,2) as distances 
+	from runner_orders r join customer_orders c
+	on r.order_id = c.order_id
+	group by customer_id;
+
+-- 5.What was the difference between the longest and shortest delivery times for all orders?
+with cte_table as(
+		select 
+			cast(
+				case 
+					when duration like '%mins' then replace(duration,'mins','')
+					when duration like '%minute' then replace(duration,'minute','')
+					when duration like '%minutes' then replace(duration,'minutes','')
+				end 
+			AS INT) as durations
+			from runner_orders
+			)
+select concat(max(durations)  - min(durations),' Min') as Long_Short_Diff
+	from cte_table;
+
+
+
+--6. What was the average speed for each runner for each 
+--delivery and do you notice any trend for these values?
+with cte_table as(
+		select 
+			runner_id,		
+			avg(
+				CAST(
+					REPLACE(distance, 'km', '') 
+					as decimal)
+				) as distances,
+			cast(
+				case 
+					when duration like '%mins' then replace(duration,'mins','')
+					when duration like '%minute' then replace(duration,'minute','')
+					when duration like '%minutes' then replace(duration,'minutes','')
+				end 
+			AS INT) as durations
+			from runner_orders
+			group by runner_id, 
+				cast(
+					case 
+						when duration like '%mins' then replace(duration,'mins','')
+						when duration like '%minute' then replace(duration,'minute','')
+						when duration like '%minutes' then replace(duration,'minutes','')
+					end 
+				AS INT)
+			)
+select 
+	runner_id, 
+	round(avg(distances/durations),2) as speed_of_Runner
+from cte_table
+group by runner_id;
+
+--7. What is the successful delivery percentage for each runner?
+select 
+	runner_id,
+	round(cast(count(duration) as decimal)/cast(count(order_id) as decimal)* 100,2) as successful_delivery_percentage
+from runner_orders
+group by runner_id
